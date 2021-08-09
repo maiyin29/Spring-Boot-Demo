bb8c077a0c29a04a383b7e0f4c24fc6b768aafc9
maiyin29 <2551256988@qq.com>
2021-08-09 11:04:21 +0800

    第五次

--- spring-boot-duyi-3-thymeleaf/src/main/java/com/duyi/bean/Guest.java
+++ spring-boot-duyi-3-thymeleaf/src/main/java/com/duyi/bean/Guest.java
@@ -4,5 +4,6 @@ public class Guest {
     public static void main(String[] args) {
         System.out.println("nihao");
         System.out.println("zhangqiling");
+        System.out.println("wuxie");
     }
 }
